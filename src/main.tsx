02fd4b280b51cced37fd0d252e953437816325db
isaia <isaiahnoelsalazar474@gmail.com>
2025-10-02 21:56:10 +0800

    test: changed new design

--- src/main.tsx
+++ src/main.tsx
@@ -10,29 +10,29 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <div style={{ width: '100%', height: '100%', position: 'relative' }}>
       <DarkVeil />
-        <SplitText
-        text="Hello!"
-        className="text-2xl font-semibold text-center"
-        delay={100}
-        duration={0.6}
-        ease="power3.out"
-        splitType="chars"
-        from={{ opacity: 0, y: 40 }}
-        to={{ opacity: 1, y: 0 }}
-        threshold={0.1}
-        rootMargin="-100px"
-        tag="h1"
-        textAlign="center"
-      />
-      {/* <App /> */}
-      <GlassSurface 
-        width={300} 
-        height={200}
-        borderRadius={24}
-        className="my-custom-class"
-      >
-        <h2>Glass Surface Content</h2>
-      </GlassSurface>
     </div>
+    <SplitText
+      text="Hello!"
+      className="text-2xl font-semibold text-center"
+      delay={100}
+      duration={0.6}
+      ease="power3.out"
+      splitType="chars"
+      from={{ opacity: 0, y: 40 }}
+      to={{ opacity: 1, y: 0 }}
+      threshold={0.1}
+      rootMargin="-100px"
+      tag="h1"
+      textAlign="center"
+    />
+    {/* <App /> */}
+    <GlassSurface 
+      width={300} 
+      height={200}
+      borderRadius={24}
+      className="my-custom-class"
+    >
+      <h2>Glass Surface Content</h2>
+    </GlassSurface>
   </StrictMode>,
 )
