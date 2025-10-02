f0eb8f1776a83154b78a7427712b63e30e63bc2b
isaia <isaiahnoelsalazar474@gmail.com>
2025-10-02 21:17:52 +0800

    test: add new component

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,24 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import GlassSurface from './GlassSurface.tsx'
+import SplitText from './SplitText.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <SplitText
+      text="Hello!"
+      className="text-2xl font-semibold text-center"
+      delay={100}
+      duration={0.6}
+      ease="power3.out"
+      splitType="chars"
+      from={{ opacity: 0, y: 40 }}
+      to={{ opacity: 1, y: 0 }}
+      threshold={0.1}
+      rootMargin="-100px"
+      textAlign="center"
+    />
+    {/* <App /> */}
     <GlassSurface 
       width={300} 
       height={200}
