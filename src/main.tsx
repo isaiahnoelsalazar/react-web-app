0bebe02ebb3d9eef9eeee2b77778e9950d09ba7d
isaia <isaiahnoelsalazar474@gmail.com>
2025-10-03 20:25:21 +0800

    update: removed import

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 // import App from './App.tsx'
 import './index.css'
 import GlassSurface from './GlassSurface.tsx'
-import SplitText from './SplitText.tsx'
+// import SplitText from './SplitText.tsx'
 import LightRays from './LightRays.tsx'
 
 createRoot(document.getElementById('root')!).render(
